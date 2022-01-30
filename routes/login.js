d2bac3248bf21659e4b21e9ab4bc2e7174db94ae
Ankit Sahoo <sahooankit833@gmail.com>
2022-01-30 06:39:53 +0530

    intial deploy commit fixed error

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const axios =require("axios");
-const jwt = require("jsonwebtoken");
 
 
 function waitTime(ms){
@@ -14,12 +13,7 @@ router.post("/",async(req,res)=>{
     try{
         await waitTime(2000);
         const user= await axios.get("https://codeforces.com/api/user.info?handles="+req.body.username);
-        const accessToken = jwt.sign({
-            username: user.data.result[0].handle,
-        }, process.env.JWT_SECRET,
-            { expiresIn: "1d" });
-        console.log(user.data);
-        res.status(200).json({...user.data,accessToken});
+        res.status(200).json(user.data);
     }catch(err){
         if(err.response){
             console.log(err.response.data);
