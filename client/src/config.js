09ce849842e419c040dcf0f59dd10c360c8a1d40
Ankit Sahoo <sahooankit833@gmail.com>
2025-01-03 21:19:05 +0530

    Added domain name in config.

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://dynamic-a2oj.herokuapp.com/api/"
+    baseURL: "https://dynamic-a2oj.onrender.com/api/"
 });
